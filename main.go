d129a31f48fd198b4460b3bae075a91c5c6876f1
David Kagan <david.kagan07@gmail.com>
2024-03-24 17:35:58 -0500

    todo: feat: basic server in place

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		if req.URL.Path != "/" {
+			http.NotFound(w, req)
+			return
+		}
+		fmt.Fprintf(w, "Welcome to the home page!")
+	})
+
+	http.ListenAndServe(":8080", mux)
+}
